322254793dfd6a2783ce4b276315f31142af950f
Aksgo <akshitgoel0412@gmail.com>
2024-09-28 23:38:32 +0530

    intial graph

--- Chart/app.py
+++ Chart/app.py
@@ -1,37 +1,23 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+import DBSCAN_classifier as model_report
 app = Flask(__name__)
 #enabled cross origin request for local develpment
 CORS(app)
-def getDbData():
-    #currently the function returns sample data instead of postgres
-    #later on we will fetch the data using sqlalchemy once added to database
-    print(os.getcwd())
-    f = open("icdata.txt", "r")
-    data = f.readlines()
-    X=[]
-    Y=[]
-    ind = 200
-    for ele in data:
-        if(ind==0):
-            break
-        ele = ele[:-1]
-        a = ele.split(" ")
-        try:
-            y = a[12]
-            x = a[3]
-            X.append(int(x))
-            Y.append(int(y))
-        except:
-            continue
-        ind-=1
-    return X,Y
-
+def getModelInfo():
+    data = model_report.returnable('.lc')
+    X = data['x']
+    Y = data['y']
+    MF = data['time_of_occurances']
+    TOC = data['time_corresponding_peak_flux']
+    return X,Y,MF,TOC
+    
+    
 @app.route('/data', methods=['GET'])
 def get_data():
-    X,Y = getDbData()
-    return jsonify(X,Y)
+    X,Y,MF,TOC = getModelInfo()
+    return jsonify(X,Y,MF,TOC)
 
 if __name__=='__main__':
     app.run(debug=True)
--- Chart/graph.js
+++ Chart/graph.js
@@ -1,75 +1,102 @@
-//using fetch API to fetch the data from app.py
 async function fetchData(){
     let response = await fetch('http://127.0.0.1:5000/data');
     let data = await response.json();
     let result = JSON.stringify(data);
     return result;
 }
+
 let data = fetchData()
-data.then(response=>{
-    plotGraph(response)
+data.then(response => {
+    plotGraph(response);
 });
-console.log(ChartZoom.version)
+
 function plotGraph(data){
-    //converting string back to json object
     data = JSON.parse(data);
-    let X = data[0]//the time values
-    let Y = data[1]//the flux values
+    let X = data[0];
+    let Y = data[1];
+    let MF = data[2]
+    let TOC = data[3]
     const ctx = document.getElementById('chart-1').getContext('2d');
+    
     const zoomGestures = {
         pan: {
-          enabled: true,
-          modifierKey: 'ctrl',
+            enabled: true,
+            modifierKey: 'ctrl',
         },
         zoom: {
-          drag: {
-            enabled: true
-          },
-          mode: 'xy',
-        },
-      };
-    const chart_1 = new Chart(ctx, 
-        {
-            type:'line',
-            data :{
-                // give the x values here
-                labels :X,
-                datasets:[
-                    { 
-                        label : 'flux',
-                        //give the y values here
-                        data:Y,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth : 1
-                    }
-                ]
+            wheel: {
+                enabled: true
             },
-            options:{
-                responsive : true,
-                scales:{
-                    x:{
-                        title : {
-                            display: true,
-                            text : "Time"
-                        }
-                    },
-                    y:{
-                        title: {
-                            display : true,
-                            text: "Flux"
-                        }
-                    }
+            mode: 'xy',
+        },
+        limits: {
+            x: { min: 0, max: X.length - 1 },
+            y: { min: Math.min(...Y) - 10, max: Math.max(...Y) + 10 },
+        }
+    };
+
+    const chart_1 = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: X,
+            datasets: [
+                {
+                    label: 'Flux',  // Line chart for flux vs time
+                    data: Y,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 0.5,
+                    radius: 1.4,
+                    type: 'line',  // Explicitly set to line
                 },
-                plugins: {
-                    zoom: zoomGestures
+                {
+                    label: 'Peak Flux',
+                    data: MF.map((mf, index) => ({ x: mf, y: TOC[index] })),
+                    backgroundColor: 'rgba(255, 80, 132, 1)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    pointRadius:6, 
+                    pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                    type: 'scatter',
+                    showLine: false 
                 }
+            ]
+        },
+        options: {
+            responsive: true,
+            animation:false,
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: "Time"
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.5)'
+                    },
+                    ticks: {
+                        maxTicksLimit: 5
+                    },
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: "Flux"
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.5)', 
+                    },
+                    ticks: {
+                        stepSize: 1000
+                    }
+                }
+            },
+            plugins: {
+                zoom: zoomGestures
             }
         }
-    );
-    const panStatus = () => zoomOptions.pan.enabled ? 'enabled' : 'disabled';
-    const zoomStatus = () => zoomOptions.zoom.drag.enabled ? 'enabled' : 'disabled';
-    document.getElementById("reset-zoom").addEventListener('click',()=>{
+    });
+
+    document.getElementById("reset-zoom").addEventListener('click', () => {
         chart_1.resetZoom();
-    })
+    });
 }
