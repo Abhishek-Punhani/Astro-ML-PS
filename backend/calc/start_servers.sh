461184489a266270c46526220d688ea6a3e594eb
nirved.mishra.eee23@itbhu.ac.in <nirved431@gmail.com>
2024-10-08 01:00:15 +0530

    connected redis online

--- backend/calc/start_servers.sh
+++ backend/calc/start_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Gunicorn for the calculation server on port 5001 with 3 workers
-gunicorn -w 3 -b localhost:5001 calc_server:app &
+gunicorn -w 3 -b [::]:5000 calc_server:app &
 
 # Wait for all background processes to complete
 wait
